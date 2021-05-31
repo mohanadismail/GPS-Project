7e7e3b66997d48791a9ec134058326093bff03cb
Mohamed Mahmoud <84942322+Mohamed5510@users.noreply.github.com>
2021-05-31 21:24:14 +0200

    Update mahmoud.h

---
+++ mahmoud.h
@@ -0,0 +1,73 @@
+#include "tm4c123gh6pm.h"
+// commands
+#define	clear 							0x01
+#define	cursor_on 					0x0F
+#define cursor_off					0x0C
+#define	increment_cursor 		0x06
+#define	display_on 					0x0E
+#define	cursor_at_1stline 	0x80
+#define	cursor_at_2ndline 	0xC0
+#define mode_8pins 					0x38
+
+// I am using all pins of port B ie, D0:D7--->B0:B7
+// control: RS--->D0 , EN--->D1 
+// connect A to VDD
+// connect RW and K to GND 
+
+void delay(unsigned long t) {
+    while(t)	t--; }
+
+void lcd_cmd(unsigned char ch){
+	GPIO_PORTD_DATA_R &=~0x01;	//RS = 0
+	GPIO_PORTB_DATA_R = ch;			//sending command
+	GPIO_PORTD_DATA_R |= 0x02;	//enable on
+	delay(100000);
+	GPIO_PORTB_DATA_R &=~0x02;	//enable off
+}
+void lcd_data(unsigned char data){
+	GPIO_PORTD_DATA_R |= 0x01;	//RS = 1
+	GPIO_PORTB_DATA_R = data;		//sending data
+	GPIO_PORTD_DATA_R |= 0x02;	//enable on
+	delay(100000);
+	GPIO_PORTD_DATA_R &=~0x02;	//enable off
+}
+void lcd_intialization(void){
+	SYSCTL_RCGCGPIO_R |= 0x0A;
+	while ( (SYSCTL_RCGCGPIO_R & 0x0A) == 0); //delay
+	
+	//B
+	GPIO_PORTB_DIR_R = 0xFF;
+	GPIO_PORTB_DEN_R = 0xFF;
+	GPIO_PORTB_AFSEL_R = 0x00;
+	GPIO_PORTB_AMSEL_R = 0x00;
+	GPIO_PORTB_PCTL_R = 0x00;
+	
+	//D
+	GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	GPIO_PORTD_CR_R |= 0x80;
+	GPIO_PORTD_DIR_R |= 0x03;
+	GPIO_PORTD_DEN_R |= 0x03;	
+	GPIO_PORTD_AFSEL_R &=~0x03;
+	GPIO_PORTD_AMSEL_R &=~ 0x03;
+	GPIO_PORTD_PCTL_R &=~ 0x03;
+}
+void lcd_ready (void){
+	lcd_cmd(mode_8pins);
+	lcd_cmd(display_on);	
+	lcd_cmd(clear);	
+	lcd_cmd(cursor_on);  
+	lcd_cmd(cursor_at_1stline);
+}
+void print(int x){
+	lcd_ready();
+	int y, z, v;
+	y = x % 10;
+	x /= 10;
+	z = x % 10;
+	x /= 10;
+	v = x;
+	lcd_data((char) v);
+	lcd_data((char) z);
+	lcd_data((char) y);
+	lcd_data('m');
+}

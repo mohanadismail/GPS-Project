e8bd87ec49454a28a5344e1ab3a47b642972ec2a
IIKhairyII <52865131+IIKhairyII@users.noreply.github.com>
2021-05-31 11:52:50 +0200

    REMOVE Test part

--- karim.c
+++ karim.c
@@ -8,7 +8,6 @@ void PORTF_init(void){
 		GPIO_PORTF_DIR_R|=0x0E;
 		GPIO_PORTF_DEN_R|= 0x0E;
 		GPIO_PORTF_AMSEL_R&= ~0x0E;
-		GPIO_PORTF_DATA_R|= 0x0E; //Just to see if the leds are on or not ===> if they are working erase this line
 }
 
 void led_on (void){

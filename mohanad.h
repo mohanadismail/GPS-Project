19ef82f44ade4e90a070dbcbaa95c05d3664bea8
Mohanad <mohanadismail789@gmail.com>
2021-06-03 15:24:19 +0200

    Added function to extract GPGGA message from rest of messages

--- mohanad.h
+++ mohanad.h
@@ -1,6 +1,6 @@
 #include <string.h>
 
-void splitUART (char str[]) {
+void split_GPGGA (char str[]) {
 	char *data[15];
   data[0] = strtok(str, ",");
   int i = 0;
@@ -8,4 +8,41 @@ void splitUART (char str[]) {
 		i++;
     data[i] = strtok(NULL, ",");
   }
+}
+
+void extract_GPGGA_message(char str[]) {
+    str[0] = '$';
+    str[1] = 'G';
+    str[2] = 'P';
+    str[3] = 'G';
+    str[4] = 'G';
+    str[5] = 'A';
+    int i;
+    while (1) {
+        char c = readUART();
+        i = 6;
+        if (c == '$') {
+            c = readUART();
+            if (c == 'G') {
+                c = readUART();
+                if (c == 'P') {
+                    c = readUART();
+                    if (c == 'G') {
+                        c = readUART();
+                        if (c == 'G') {
+                            c = readUART();
+                            if (c == 'A') {
+                                while (c != '\n') {
+                                    c = readUART();
+                                    str[i] = c;
+                                    i++;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }

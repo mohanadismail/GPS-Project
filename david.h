e278983ac64872ea5aa19db101a4547090518a13
davidwagih <78867146+david-wagih@users.noreply.github.com>
2021-06-03 14:56:02 +0200

    second update
    changing some conditions in the validity function

--- david.h
+++ david.h
@@ -1,33 +1,35 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-float getLatitude(char *data[])
-{
-    if (!strcmp(data[0], "$GPGGA"))
-    {
-        return strtof(data[2], NULL);
-    }
+float getLatitude(char *data[]) {
+    
+  if (!strcmp(data[0], "$GPGGA")) {
+      
+    return strtof(data[2], NULL);
+  }
 }
 
-float getLongitude(char *data[])
-{
-    if (!strcmp(data[0], "$GPGGA"))
-    {
-        return strtof(data[4], NULL);
-    }
+float getLongitude(char *data[]) {
+    
+  if (!strcmp(data[0], "$GPGGA")) {
+      
+    return strtof(data[4], NULL);
+  }
 }
-int validity(char *data[])
-{
-    if (!strcmp(data[0], "$GPGGA"))
-    {
-        if (*data[6] == '1')
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
+
+
+int validity(char *data[]) {
+    
+  if (!strcmp(data[0], "$GPGGA")) {
+      
+    if (*data[6] <= '0') {
+        
+      return 0;
+        
+    } else if ('1' <= *data[6] && *data[6] <= '5') {
+        
+      return 1;
     }
+  }
 }

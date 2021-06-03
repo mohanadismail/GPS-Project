024f8b352a99b69dd2a1628a2b1f9ab95190ad39
Mohanad <mohanadismail789@gmail.com>
2021-06-03 15:28:43 +0200

    Added functions documentation

--- mohanad.h
+++ mohanad.h
@@ -1,6 +1,9 @@
 #include <string.h>
 
 void split_GPGGA (char str[]) {
+	//str[] is a global variable
+	//This function takes the GPGGA message from str[] and splits it into tokens
+	//to easily extract quality indicator, latitude and longitude
 	char *data[15];
   data[0] = strtok(str, ",");
   int i = 0;
@@ -11,6 +14,9 @@ void split_GPGGA (char str[]) {
 }
 
 void extract_GPGGA_message(char str[]) {
+	//str[] is a global variable
+	//This function keeps on receiving input from the UART until
+	//it detects the GPGGA message, then stores the message in str[]
     str[0] = '$';
     str[1] = 'G';
     str[2] = 'P';

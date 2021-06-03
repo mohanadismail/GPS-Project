b68772badaf27dff1102c85de8a187040c0e02bf
Mohanad <mohanadismail789@gmail.com>
2021-06-03 20:12:44 +0200

    Included all team members header files

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "Headers/tm4c123gh6pm.h"
 #include "karim.h"
 #include "7_segments.h"
-#include "mohanad.h"
+#include "GPGGA_functions.h"
 #include "david.h"
-
-
+#include "distanceBetweenTwoPoints.h"
+#include "UART.h"
 
 
 int main () {

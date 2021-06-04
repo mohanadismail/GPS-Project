e8a96595428dbc1d7f536864727450aad67114f8
Mohanad <mohanadismail789@gmail.com>
2021-06-04 10:42:01 +0200

    Added saving current location and calculating distance

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "distanceBetweenTwoPoints.h"
 
 float distance = 0;
+float prev_lat, prev_long, current_lat, current_long;
 
 int main () {
 	//All initilizations
@@ -18,5 +19,14 @@ int main () {
 	while (!validity(data)) { //wait till gps fix
 		extract_GPGGA_message();
 		split_GPGGA();
+	}
+	prev_lat = getLatitude(data); //save current location
+	prev_long = getLongitude(data);
+	while (1) {
+		extract_GPGGA_message(); //take new input
+		split_GPGGA();
+		current_lat = getLatitude(data);
+		current_long = getLongitude(data);
+		distance += distanceBetweenTwoPoints(prev_lat, prev_long, current_lat, current_long); //calculate distance and increase calculated distance
 	}
 }

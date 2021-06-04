ed939aaec9cbc2c55f4c20aeaf9c7e9842d12634
Mohanad <mohanadismail789@gmail.com>
2021-06-04 13:30:35 +0200

    Added delay between each input

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ float prev_lat, prev_long, current_lat, current_long;
 char str[150]; //(externed in GPGGA_functions.h) (to save GPGGA message in)
 char *data[15]; //(externed in GPGGA_functions.h) (to split GPGGA into tokens)
 int data_length; //(externed in GPGGA_function.h) (to monitor data length and avoid segmentation fault)
+int delay;
 
 int main () {
 	//All initilizations
@@ -45,5 +46,9 @@ int main () {
 		current_long = getLongitude(data);
 		distance += distanceBetweenTwoPoints(prev_lat, prev_long, current_lat, current_long); //calculate distance and increase calculated distance
 		print((int)distance); //show distance on seven segments while moving
+		delay = 10000;
+		while (delay > 0) {
+			delay--;
+		}
 	}
 }

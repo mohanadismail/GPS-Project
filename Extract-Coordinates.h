5f328e9e94c5e85e015af5e35ef69f0ccb88b0d6
davidwagih <78867146+david-wagih@users.noreply.github.com>
2021-06-04 11:03:55 +0200

    Fixing Some Errors

--- Extract-Coordinates.h
+++ Extract-Coordinates.h
@@ -2,34 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-float getLatitude(char *data[]) {
-    
-  if (!strcmp(data[0], "$GPGGA")) {
-      
-    return strtof(data[2], NULL);
-  }
-}
+float getLatitude(char *data[]) { return strtof(data[2], NULL); }
 
-float getLongitude(char *data[]) {
-    
-  if (!strcmp(data[0], "$GPGGA")) {
-      
-    return strtof(data[4], NULL);
-  }
-}
-
+float getLongitude(char *data[]) { return strtof(data[4], NULL); }
 
 int validity(char *data[]) {
     
-  if (!strcmp(data[0], "$GPGGA")) {
+  if (*data[6] <= '0') {
       
-    if (*data[6] <= '0') {
-        
-      return 0;
-        
-    } else if ('1' <= *data[6] && *data[6] <= '5') {
-        
-      return 1;
-    }
+    return 0;
+      
+  } else if ('1' <= *data[6] && *data[6] <= '5') {
+      
+    return 1;
+      
+  } else {      
+    return 0;
   }
 }
+

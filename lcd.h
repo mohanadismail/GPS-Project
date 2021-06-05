2cb1db1591a0fccae6a937a14298ea9009880c2a
Mohamed Mahmoud <84942322+Mohamed5510@users.noreply.github.com>
2021-06-05 21:39:11 +0200

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -10,7 +10,7 @@
 #define mode_8pins 					0x38
 
 // I am using all pins of port B ie, D0:D7--->B0:B7
-// control: RS--->D0 , EN--->D1 
+// control: RS--->A2 , EN--->A3 
 // connect A to VDD
 // connect RW and K to GND 
 
@@ -18,22 +18,22 @@ void delay(unsigned long t) {
     while(t)	t--; }
 
 void lcd_cmd(unsigned char ch){
-	GPIO_PORTD_DATA_R &=~0x01;	//RS = 0
+	GPIO_PORTA_DATA_R &=~0x04;	//RS = 0
 	GPIO_PORTB_DATA_R = ch;			//sending command
-	GPIO_PORTD_DATA_R |= 0x02;	//enable on
-	delay(10000);
-	GPIO_PORTB_DATA_R &=~0x02;	//enable off
+	GPIO_PORTA_DATA_R |= 0x08;	//enable on
+	delay(100000);
+	GPIO_PORTA_DATA_R &=~0x08;	//enable off
 }
 void lcd_data(unsigned char data){
-	GPIO_PORTD_DATA_R |= 0x01;	//RS = 1
+	GPIO_PORTA_DATA_R |= 0x04;	//RS = 1
 	GPIO_PORTB_DATA_R = data;		//sending data
-	GPIO_PORTD_DATA_R |= 0x02;	//enable on
-	delay(10000);
-	GPIO_PORTD_DATA_R &=~0x02;	//enable off
+	GPIO_PORTA_DATA_R |= 0x08;	//enable on
+	delay(100000);
+	GPIO_PORTA_DATA_R &=~0x08;	//enable off
 }
 void lcd_initialization(void){
-	SYSCTL_RCGCGPIO_R |= 0x0A;
-	while ( (SYSCTL_RCGCGPIO_R & 0x0A) == 0);
+	SYSCTL_RCGCGPIO_R |= 0x03;
+	while ( (SYSCTL_RCGCGPIO_R & 0x03) == 0);
 	
 	//B
 	GPIO_PORTB_DIR_R = 0xFF;
@@ -42,25 +42,23 @@ void lcd_initialization(void){
 	GPIO_PORTB_AMSEL_R = 0x00;
 	GPIO_PORTB_PCTL_R = 0x00000000;
 	
-	//D
-	
-	GPIO_PORTD_DIR_R |= 0x03;
-	GPIO_PORTD_DEN_R |= 0x03;	
-	GPIO_PORTD_AFSEL_R &=~0x03;
-	GPIO_PORTD_AMSEL_R &=~ 0x03;
-	GPIO_PORTB_PCTL_R &= ~ 0x000000FF;
+	//A
+	GPIO_PORTA_DIR_R |= 0x0C;
+	GPIO_PORTA_DEN_R |= 0x0C;	
+	GPIO_PORTA_AFSEL_R &=~0x0C;
+	GPIO_PORTA_AMSEL_R &=~ 0x0C;
+	GPIO_PORTA_PCTL_R &= ~ 0x0000FF00;
 
 }
 void lcd_ready (void){
 	lcd_cmd(mode_8pins);
 	lcd_cmd(display_on);	
-	lcd_cmd(clear);	
-	lcd_cmd(cursor_on);  
-	lcd_cmd(cursor_at_1stline);
+	lcd_cmd(increment_cursor);  
+	lcd_cmd(clear);
 }
 void print(int x){
-	lcd_ready();
 	int y, z, v;
+	lcd_ready();
 	y = x % 10;
 	x /= 10;
 	z = x % 10;

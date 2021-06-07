ce73c376b30bde4f4e1ad66d5922eca6703b6e97
MMostafa-Hub <72346762+MMostafa-Hub@users.noreply.github.com>
2021-06-07 20:39:33 +0200

    Refactoring

--- distanceBetweenTwoPoints.h
+++ distanceBetweenTwoPoints.h
@@ -1,24 +1,24 @@
 #include <math.h>
 #define rad(x) x*(3.14159/180)
+#define DDM_to_DD(x) (int)(x / 100) + ((x - ((int)(x / 100)) * 100) / 60)
 
-double rad_lat_1, rad_long_1, rad_lat_2, rad_long_2,lat_diff, long_diff, under_sqrt, flt_sqrt, haversineDistance;
+double lat_diff, long_diff, under_sqrt, flt_sqrt, haversineDistance;
 int D_EarthDiameter = 2 * 6371000;
 
 double distanceBetweenTwoPoints(double lat_1, double long_1, double lat_2, double long_2) // Haversine Formula
-{ 
-	// converting DDM to DD
-	lat_1 = (int)(lat_1 / 100) + (lat_1 - ((int)(lat_1 / 100))*100)/60;
-  	long_1 = (int)(long_1 / 100) + (long_1 - ((int)(long_1 / 100))*100)/60;
-  	lat_2 = (int)(lat_2 / 100) + (lat_2 - ((int)(lat_2 / 100))*100)/60;
-  	long_2 = (int)(long_2 / 100) + (long_2 - ((int)(long_2 / 100))*100)/60;
-	// converting Coordinates degree values into radian
-	rad_lat_1 = rad(lat_1) , rad_lat_2 = rad(lat_2);
-	rad_long_1 = rad(long_1) , rad_long_2 = rad(long_2);
+{
+	// converting DDM into DD
+	lat_1 = DDM_to_DD(lat_1), long_1 = DDM_to_DD(long_1);
+	lat_2 = DDM_to_DD(lat_2), long_2 = DDM_to_DD(long_2);
 
+	// converting from degrees into radian
+	lat_1 = rad(lat_1), long_1 = rad(long_1);
+	lat_2 = rad(lat_2), long_2 = rad(long_2);
 
-	lat_diff = rad_lat_2 - rad_lat_1;
-	long_diff = rad_long_2 - rad_long_1;
-	under_sqrt = pow(sin(lat_diff / 2), 2) + cos(rad_lat_1)*cos(rad_lat_2)*pow(sin(long_diff / 2), 2);
+	// implementing Haversine 
+	lat_diff = lat_2 - lat_1;
+	long_diff = long_2 - long_1;
+	under_sqrt = pow(sin(lat_diff / 2), 2) + cos(lat_1)*cos(lat_2)*pow(sin(long_diff / 2), 2);
 	flt_sqrt = sqrtf(under_sqrt);
 	haversineDistance = D_EarthDiameter * asinf(flt_sqrt);
 

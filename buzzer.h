f4cca15b7d0760473ccae05eccee53813ad273c9
Mohamed Mahmoud <84942322+Mohamed5510@users.noreply.github.com>
2021-06-11 13:33:31 +0200

    Update buzzer.h

--- buzzer.h
+++ buzzer.h
@@ -2,22 +2,22 @@
 
 void buzzer_initialize(){  
 	
-	// B5........00100000
-	SYSCTL_RCGCGPIO_R |= 0x02;        
-	while ((SYSCTL_PRGPIO_R & 0x02)==0);  
-	GPIO_PORTB_DIR_R |= 0x20; 
-	GPIO_PORTB_DEN_R |= 0x20;
-	GPIO_PORTB_AMSEL_R &= ~ 0x20; 
-	GPIO_PORTB_AFSEL_R &= ~ 0x20;
-	GPIO_PORTB_PCTL_R &= ~ 0x00F00000;
+	// A5........00100000
+	SYSCTL_RCGCGPIO_R |= 0x01;        
+	while ((SYSCTL_PRGPIO_R & 0x01)==0);  
+	GPIO_PORTA_DIR_R |= 0x20; 
+	GPIO_PORTA_DEN_R |= 0x20;
+	GPIO_PORTA_AMSEL_R &= ~ 0x20; 
+	GPIO_PORTA_AFSEL_R &= ~ 0x20;
+	GPIO_PORTA_PCTL_R &= ~ 0x00F00000;
 }
 void buzzer_on(void){
 	unsigned long i;
 	buzzer_initialize();
 	while(1){
-	GPIO_PORTB_DATA_R |= 0x20;
+	GPIO_PORTA_DATA_R |= 0x20;
 	for(i = 1000000 ; i > 0 ; i--){}
-	GPIO_PORTB_DATA_R &= ~0x20;
+	GPIO_PORTA_DATA_R &= ~0x20;
 	for(i = 1000000 ; i > 0 ; i--){}
 	}
 }
